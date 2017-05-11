81caf6ce37a8402951bd232766e0f80f22225e8b
Kyle Armstrong <k90armstrong@gmail.com>
2017-05-11 20:06:36 +0000

    starting to clean up the code, still needs work

--- recipe_app_express/public/javascripts/menu.js
+++ recipe_app_express/public/javascripts/menu.js
@@ -9,19 +9,21 @@ $(document).ready(function() {
   var breakfasts = [];
   var desserts = [];
   var tdNode;
-  
+  var mealType;
+  var day;
+  var recipes;
+  var currentMealType;
+  var autocomplete = {};
+  var dmenu = null; // change this to use the selected menu from the cookie??
   var mealTypeEnum = {
     breakfast: '1',
     lunch: '2',
     dinner: '3',
-    snack: '4',
-    dessert: '5'
+    snack1: '4',
+    snack2: '5',
+    dessert: '6'
   }
-  
   var mealSelectElem = $("#mealSelection");
-  
- 
-  
   
   // Initialize Firebase
   var config = {
@@ -37,12 +39,83 @@ $(document).ready(function() {
   var database = firebase.database();
   // var userId = firebase.auth().currentUser.uid;
   var userId = "LSJipcKH0URTIuDUkGNZNoe34Lb2";
-  var recipesRef = firebase.database().ref('/users/' + userId + '/recipes');
   
+  // Functions start HERE __________________________________________
+  // Function to create an option for the modal, this is what a user will 
+  // Select from when they go to the menu.
+  // This function will also add them to the element
+  function addOptionsToSelect(){  // this should probably all be refactored!!
+    var options_obj;
+    tdNode = this
+    var trNode = tdNode.parentElement;
+    var tableCols = trNode.children;
+    // get the day that was clicked
+    for (var i=0; i<tableCols.length; i++){
+      if (tableCols[i] === tdNode){
+        day = i-1;
+        break;
+      }
+    }
+
+    // Figure out which was clicked
+    var classList = this.className.split(/\s+/);
+    for (var i = 0; i < classList.length; i++) {
+    switch (classList[i]){
+        case "breakfast":
+          options_obj = breakfasts;
+          mealType = "Breakfast";
+          currentMealType = mealTypeEnum.breakfast;
+          break;
+        case "lunch":
+          options_obj = lunches;
+          mealType = "Lunch";
+          currentMealType = mealTypeEnum.lunch;
+          break;
+        case "dinner":
+          options_obj = dinners;
+          mealType = "Dinner";
+          currentMealType = mealTypeEnum.dinner;
+          break;
+        case "snack1":
+          options_obj = snacks;
+          mealType = "Snack";
+          currentMealType = mealTypeEnum.snack1;
+          break;
+        case "snack2":
+          options_obj = snacks;
+          mealType = "Snack";
+          currentMealType = mealTypeEnum.snack2;
+          break;
+        case "dessert":
+          options_obj = desserts;
+          mealType = "Dessert";
+          currentMealType = mealTypeEnum.dessert;
+          break;
+      }
+  }
+    var newOption;
+    var optionsHtml = '';
+    // create meal options to select in dropdown
+    for (key in options_obj){
+      newOption = "<option value='" + options_obj[key].id + "'>" + options_obj[key].name + "</option>";
+      optionsHtml += newOption;
+    }
+    $("#mealSelection").empty();
+    $("#mealSelection").append(optionsHtml);
+    $("#mealType").html(mealType);
+    
+}
+  // Functions end HERE __________________________________________
+  
+  var recipesRef = firebase.database().ref('/users/' + userId + '/recipes');
   // Getting all recipes and putting them in the right list
   recipesRef.once('value').then(function(snapshot) {
-  var recipes = snapshot.val();
+  recipes = snapshot.val();
+  var index = 0;
+  var recipe_length = Object.keys(recipes).length;
+  var keys = '';
   for (key in recipes){
+    keys = recipes[key].name;
     switch (recipes[key].type){
       case mealTypeEnum.breakfast:
         breakfasts.push(recipes[key]);
@@ -53,18 +126,36 @@ $(document).ready(function() {
       case mealTypeEnum.dinner:
         dinners.push(recipes[key]);
         break;
-      case mealTypeEnum.snack:
+      case mealTypeEnum.snack1:
+        snacks.push(recipes[key]);
+        break;
+      case mealTypeEnum.snack2:
         snacks.push(recipes[key]);
         break;
       case mealTypeEnum.dessert:
         desserts.push(recipes[key]);
         break;
     }
+
+    autocomplete[keys] = null;
   }
-  console.log(breakfasts);
-  console.log(lunches);
-  console.log(dinners);
+  autocomplete = {"apple pie" : null, "cranberry" : null};
   });
+  
+  // for the search feature (need to change this probably to something better)
+  $('input.autocomplete').autocomplete({
+    data: {"apple pie" : null, "cranberry" : null},
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  });
+  //add onclick even to add buttons
+  $("#save_btn").click(addOptionsToSelect);
+  $(".breakfast").click(addOptionsToSelect);
+  $(".lunch").click(addOptionsToSelect);
+  $(".dinner").click(addOptionsToSelect);
+  $(".snack1").click(addOptionsToSelect);
+  $(".snack2").click(addOptionsToSelect);
+  $(".dessert").click(addOptionsToSelect);
+  $("#recipeDoneButton").click(recipeAddClick);
 
   // Class to represent a row in the seat reservations grid
   function mealtemp(recipe) {
@@ -73,86 +164,42 @@ $(document).ready(function() {
       self.recipe = ko.observable(recipe);
   }
   
-  // Function to create an option for the modal, this is what a user will 
-  // Select from when they go to the menu.
-  // This function will also add them to the element
-  function addOptionsToSelect(){
-    var options_obj;
-    var mealType;
-    tdNode = this
-    console.log(this);
-    // Figure out which was clicked
-    var classList = this.className.split(/\s+/);
-    for (var i = 0; i < classList.length; i++) {
-    switch (classList[i]){
-        case "breakfast":
-          options_obj = breakfasts;
-          mealType = "Breakfast";
-          break;
-        case "lunch":
-          options_obj = lunches;
-          mealType = "Lunch";
-          break;
-        case "dinner":
-          options_obj = dinners;
-          mealType = "Dinner";
-          break;
-        case "snack":
-          options_obj = snacks;
-          mealType = "Snack";
-          break;
-        case "dessert":
-          options_obj = desserts;
-          mealType = "Dessert";
-          break;
-      }
-  }
-    //create array of strings for the options
-    var options = [];
-    for (key in options_obj){
-      options.push(options_obj[key].name);
-    }
-
-    var newOption;
-    var optionsHtml = '';
-    
-    for (var i = 0; i < options.length; i++){
-      newOption = "<option>" + options[i] + "</option>";
-      optionsHtml += newOption;
-    }
-    $("#mealSelection").empty();
-    $("#mealSelection").append(optionsHtml);
-    $("#mealType").html(mealType);
-    
-}
+  
 
   // Overall viewmodel for this screen, along with initial state
   function MenuViewModel() {
       var self = this;
-
-      // Non-editable catalog data - would come from the server
       self.availableMeals = dinners;
-
-      // Editable data
-      //self.seats = ko.observableArray([
-      //  new SeatReservation("Steve", self.availableMeals[0]),
-      //new SeatReservation("Bert", self.availableMeals[0])
-      //]);
   }
   
   function menu(){
     var self = this;
-    self.breakfasts = []; // [sun, mon, tue, wed, thur, fri, sat]
-    self.lunches = [];
-    self.dinners = [];
-    self.snacks1 = [];
-    self.snacks2 = [];
-    self.desserts = [];  // meal_obj, 
+    self.id = '';
+    self.breakfasts = [{},{},{},{},{},{},{}]; // [sun, mon, tue, wed, thur, fri, sat]
+    self.lunches = [{},{},{},{},{},{},{}];
+    self.dinners = [{},{},{},{},{},{},{}];
+    self.snacks1 = [{},{},{},{},{},{},{}];
+    self.snacks2 = [{},{},{},{},{},{},{}];
+    self.desserts = [{},{},{},{},{},{},{}];  // meal_obj = meal, meal_type=mealTypeEnum,index (0=sun)
     self.addMeal = function(meal, meal_type, day){
-      
-      
+      switch(meal_type){
+        case mealTypeEnum.breakfast:
+          self.breakfasts[day] = meal;
+        case mealTypeEnum.lunch:
+          self.lunches[day] = meal;
+        case mealTypeEnum.dinner:
+          self.dinners[day] = meal;
+        case mealTypeEnum.snack1:
+          self.snacks1[day] = meal; //TODO fix the snacks...enum is only one snack
+        case mealTypeEnum.snack2:
+          self.snacks2[day] = meal;
+        case mealTypeEnum.dessert:
+          self.desserts[day] = meal;
+      }
     }
   }
+  
+  var currentMenu = new menu();
   
   // This will create an object that will store the 
   // recipe and the servings that are selected
@@ -164,7 +211,7 @@ $(document).ready(function() {
   
   // Function to take an element and change it's image to a photo instead of the +
   function changeToPhoto(photo_file, element){
-    element.innerHTML = '<img class="editable" src="images/' + photo_file +'">';
+    element.innerHTML = '<img class="editable circle responsive-img" src="images/' + photo_file +'"' + 'width="50" height="50"' + '>';
   }
   
   
@@ -173,16 +220,17 @@ $(document).ready(function() {
     // Get the image from the database url or use a default based on type of meal
     var photo_file = "food.jpg"
     changeToPhoto(photo_file, tdNode);
+    // Get the right meal obj
+    var mealId = $('#mealSelection').find(":selected").val();
+    for (key in recipes){
+      if(mealId == recipes[key].id){
+        //update menu
+        currentMenu.addMeal(recipes[key], currentMealType, day);
+        break;
+      }
+    }
   }
   
-   //add onclick even to add buttons
-  $(".breakfast").click(addOptionsToSelect);
-  $(".lunch").click(addOptionsToSelect);
-  $(".dinner").click(addOptionsToSelect);
-  $(".snack").click(addOptionsToSelect);
-  $(".dessert").click(addOptionsToSelect);
-  $("#recipeDoneButton").click(recipeAddClick);
-
   ko.applyBindings(new MenuViewModel());
 });
       
--- recipe_app_express/public/javascripts/recipes.js
+++ recipe_app_express/public/javascripts/recipes.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
     
   // Initialize all variables that will be used
   var dinners = [];
@@ -8,7 +8,6 @@ $(document).ready(function(){
   var snacks = [];
   var breakfasts = [];
   var desserts = [];
-  
   var mealTypeEnum = {
     breakfast: '1',
     lunch: '2',
@@ -16,25 +15,168 @@ $(document).ready(function(){
     snack: '4',
     dessert: '5'
   }
+  var ingredients;
+  var userId = "LSJipcKH0URTIuDUkGNZNoe34Lb2"; // this is my user ID need to figure out login/sesion
     
-    // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyDzN2pLq28MXI-ZloJcIp9iUTbhdaUOVRw",
-        authDomain: "recipesaver-c7f4d.firebaseapp.com",
-        databaseURL: "https://recipesaver-c7f4d.firebaseio.com",
-        storageBucket: "recipesaver-c7f4d.appspot.com",
-        messagingSenderId: "163762066965"
-      };
-      firebase.initializeApp(config);
-     // Get a reference to the database service
-    var database = firebase.database();
-    var ingredients;
-    
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDzN2pLq28MXI-ZloJcIp9iUTbhdaUOVRw",
+    authDomain: "recipesaver-c7f4d.firebaseapp.com",
+    databaseURL: "https://recipesaver-c7f4d.firebaseio.com",
+    storageBucket: "recipesaver-c7f4d.appspot.com",
+    messagingSenderId: "163762066965"
+  };
+  firebase.initializeApp(config);
+ // Get a reference to the database service
   var database = firebase.database();
-  // var userId = firebase.auth().currentUser.uid;
-  var userId = "LSJipcKH0URTIuDUkGNZNoe34Lb2";
-  var recipesRef = firebase.database().ref('/users/' + userId + '/recipes');
   
+  // firebase.onAuth(function(authData){
+  //   if (authData == null){
+  //     var provider = new firebase.auth.GoogleAuthProvider();
+  //   firebase.auth().signInWithRedirect(provider); // redirect to sign in
+  //   }
+  //   else{
+  //     console.log('logged in');
+  //   }
+  // });
+  
+  // firebase.auth().onAuthStateChanged(function(user){
+  //   console.log(user);
+  // });
+    
+  
+  
+  // var userId = firebase.auth().currentUser
+  // // Start the provider for signing in
+  // if (firebase.auth().currentUser == null){
+  //   var provider = new firebase.auth.GoogleAuthProvider();
+  //   firebase.auth().signInWithRedirect(provider); // redirect to sign in
+  // }
+
+  //console.log(userId);
+  
+  
+  // Functions Start HERE____________________________________________
+  function ingredientForDB(name, qty, unit){
+    var self = this;
+    self.name = name;
+    self.qty = qty;
+    self.unit = unit;
+    
+  }
+
+  var addRow = function (){
+    var row = document.createElement('div');
+    row.className = 'row';
+    document.getElementById('container').appendChild(row);
+    return row;
+    
+  }
+
+  var addRecipeCard = function(recipe_obj, rowEl){
+    console.log(recipe_obj);
+    var recipe_obj_image = ''; // set to default if no image in recipe_obj
+    var col = document.createElement('div');
+    col.className = "col s4";
+    var card = document.createElement('div');
+    card.className = 'card';
+    var cardImage = document.createElement('div');
+    cardImage.className = 'card-image recipe-image';
+    var img = document.createElement('img');
+    img.src = "http://lorempixel.com/125/125/food/1";
+    var stacked = document.createElement('div');
+    stacked.className = 'card-stacked';
+    var cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
+    var span = document.createElement('span');
+    span.className = 'card-title';
+    span.innerHTML = recipe_obj.name;
+    var p1 = document.createElement('p');
+    p1.innerHTML = 'Prep Time: ' + recipe_obj.preptime;
+    var p2 = document.createElement('p');
+    p2.innerHTML = 'Cook Time: ' + recipe_obj.cooktime;
+    var action = document.createElement('div');
+    action.className = 'card-action';
+    var view = document.createElement('a');
+    view.className = 'view';
+    view.innerHTML = 'View';
+    var add = document.createElement('a');
+    add.className = 'add-button';
+    var icon = document.createElement('i');
+    icon.className = 'material-icons';
+    icon.innerHTML = 'add';
+    
+    col.appendChild(card);
+    card.appendChild(cardImage);
+    cardImage.appendChild(img);
+    card.appendChild(stacked);
+    stacked.appendChild(cardContent);
+    cardContent.appendChild(span);
+    cardContent.appendChild(p1);
+    cardContent.appendChild(p2);
+    stacked.appendChild(action);
+    action.appendChild(view);
+    action.appendChild(add);
+    add.appendChild(icon);
+    // add click event to add and view
+    
+    rowEl.appendChild(col); // add row and card to the row
+  }
+  
+  function writeUserData(userId, ingredients, recipe_name, recipe_type, directions, prep_time, cook_time) {
+    console.log(ingredients);
+    // Get a key for a new recipe.
+    var newRecipeKey = firebase.database().ref().child('users/' + userId + '/recipes').push().key;
+    console.log(newRecipeKey);
+    var recipe = {
+      name: recipe_name, 
+      type: recipe_type,
+      directions: directions, 
+      image: "image url here",
+      ingredients: ingredients,
+      preptime: prep_time,
+      cooktime: cook_time,
+      id: newRecipeKey
+     };
+    
+    var updates = {};
+    updates['users/' + userId + '/recipes/' + newRecipeKey] = recipe;
+    firebase.database().ref().update(updates);
+    // add error handling
+  }
+  
+  function addButtonClicked(){
+    console.log("add button clicked");
+    writeUserData(userId, ingredients(), $("#recipe_name").val(), 
+                  $("#recipe_type").val(), $("#directions").val(),
+                  $("#prep_time").val(), $("#cook_time").val());
+    console.log("add button clicked");
+  }
+  
+  function clearRecipeButtonClicked(){
+    console.log("clear button clicked");
+    // go through all of the modal to add recipe and clear all entries
+    $("#directions").val('');
+    $("#cook_time").val('');
+    $("#prep_time").val('');
+    $("#recipe_type option[value='5']").prop('selected', true);
+    $('#recipe_name').val('');
+    $("#directions").trigger('autoresize');
+    $("#cook_time").trigger('autoresize');
+    $("#prep_time").trigger('autoresize');
+    $('#recipe_name').trigger('autoresize');
+    
+  }
+
+  // Functions END HERE ____________________________________________
+  
+  // add event handlers ____________________________________________
+  $("#addRecipeButton").click(addButtonClicked);
+  $("#clearRecipeButton").click(clearRecipeButtonClicked);
+  // end add event handlers_________________________________________
+  
+  
+  var recipesRef = firebase.database().ref('/users/' + userId + '/recipes');
   // Getting all recipes and putting them in the right list
   recipesRef.once('value').then(function(snapshot) {
   var recipes = snapshot.val();
@@ -57,65 +199,26 @@ $(document).ready(function(){
         break;
     }
   }
-  console.log(breakfasts);
-  console.log(lunches);
-  console.log(dinners);
-  //Insert recipes into the html doc
-  console.log('these are the dinners', dinners);
-  console.log(dinners.length);
+  // adding all of the dinners to the html TODO add other mealtypes
+  var row = addRow();
   for (var i = 0; i < dinners.length; i++){
-    addRecipeCard(dinners[i]);
-}
+    if (i % 3 == 0){
+      row = addRow();
+    }
+    addRecipeCard(dinners[i], row);
+    }
   });
     
-    
-        
-function ingredientForDB(name, qty, unit){
-  var self = this;
-  self.name = name;
-  self.qty = qty;
-  self.unit = unit;
-  
-}
+  // Class to represent a row in the ingredients
+  function Ingredient(name, qty, unit) {
+      var self = this;
+      self.name = ko.observable(name);
+      self.qty = ko.observable(qty);
+      self.unit = ko.observable(unit);
+  }
 
-var addRecipeCard = function(recipe_obj){
-  var recipe_obj_image = ''; // set to default if no image in recipe_obj
-  var html = 
-    '<div class="col s12 m7">' +
-    '<h2 class="header">' + recipe_obj.name + '</h2>' +
-    '<div class="card horizontal">' +
-      '<div class="card-image">' +
-        //'<img src=' + recipe_obj_image +'>' + // add image url here
-      '</div>' +
-      '<div class="card-stacked">' +
-        '<div class="card-content">' +
-          '<p>' + recipe_obj.directions +'</p>' +
-        '</div>' +
-        '<div class="card-action">' +
-          '<a href="#">View</a>' +
-        '</div>' +
-      '</div>' +
-    '</div>' +
-  '</div>'
-  $('#container').append(html);
-  console.log(html);
-}
-    
-// Class to represent a row in the ingredients
-function Ingredient(name, qty, unit) {
-    var self = this;
-    self.name = ko.observable(name);
-    self.qty = ko.observable(qty);
-    self.unit = ko.observable(unit);
-
-    //self.formattedPrice = ko.computed(function() {
-      //  var price = self.meal().price;
-        //return price ? "$" + price.toFixed(2) : "None";        
-    //});    
-}
-
-// Overall viewmodel for this screen, along with initial state
-function IngredientsViewModel() {
+  // Overall viewmodel for this screen, along with initial state
+  function IngredientsViewModel() {
     var self = this;
     self.name = "indredients";
 
@@ -172,45 +275,10 @@ function IngredientsViewModel() {
     });
     
     
-}
+  }
 
 ko.applyBindings(new IngredientsViewModel());
 
-var userId = "LSJipcKH0URTIuDUkGNZNoe34Lb2";  //TODO REPLACE WITH USERS ID FROM FIREBASE
 
-
-
-function addButtonClicked(){
-    console.log("add button clicked");
-    writeUserData(userId, ingredients(), $("#recipe_name").val(), 
-                  $("#recipe_type").val(), $("#directions").val());
-    console.log("add button clicked");
-}
-
-
-function writeUserData(userId, ingredients, recipe_name, recipe_type, directions) {
-  console.log(ingredients);
-  // Get a key for a new recipe.
-  var newRecipeKey = firebase.database().ref().child('users/' + userId + '/recipes').push().key;
-  console.log(newRecipeKey);
-  var recipe = {
-    name: recipe_name, 
-    type: recipe_type,
-    directions: directions, 
-    image: "image url here",
-    ingredients: ingredients
-   };
-  
-  var updates = {};
-  updates['users/' + userId + '/recipes/' + newRecipeKey] = recipe;
-  firebase.database().ref().update(updates);
-  
-  
-  console.log('wrote to database');
-}
-
-$("#addRecipeButton").click(addButtonClicked);
-
-    
 });
   
